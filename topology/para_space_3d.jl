681f095f414510aaaa8f511d5d8f310b591a8f82
Jakob Wagner <55949397+JakobWagnerKappel@users.noreply.github.com>
2023-06-02 10:19:15 +0200

    Update para_space_3d.jl
    labels

--- topology/para_space_3d.jl
+++ topology/para_space_3d.jl
@@ -101,7 +101,7 @@ pmap(jj -> pmap(kk -> calc(jj,kk), 1:y_vec_length), 1:x_vec_length)
 z_mat = amplitude_alpha_N
 #println(amplitude_alpha_N)
 #pcolor(x_vec, y_vec, z_mat, cmap="inferno")
-plt = plot(x_vec, y_vec, z_mat, yaxis=:log, st=:surface)
+plt = plot(x_vec, y_vec, z_mat, yaxis=:log, st=:surface, title = "Limit of α_{N}/α_{Np} over U and δ", xlabel = "δ", ylabel = "U", zlabel = "α_{N}/α_{Np}")
 savefig(plt, "plt-3d-test")
 display(plt)
 println("Key")
